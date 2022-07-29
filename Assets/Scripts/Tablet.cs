6ca52bbeacaed65b9947da4adebbcf9e233389a3
relisqu <kopkopkop47@gmail.com>
2022-07-29 21:00:22 +0300

    added tablets

--- Assets/Scripts/Tablet.cs
+++ Assets/Scripts/Tablet.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Linq;
 using DG.Tweening;
 using Player_Scripts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Tablet : MonoBehaviour
 {
     [SerializeField] private Transform TextTransform;
+    [SerializeField] private TMPro.TMP_Text Text;
     [SerializeField] private float TextSpeed;
+    [SerializeField] private float TextCorruptionValue;
     private PlayerMovement _player;
 
     private void Awake()
@@ -38,5 +42,47 @@ public class Tablet : MonoBehaviour
     private void HideText()
     {
         TextTransform.DOScaleY(0f, TextSpeed).SetEase(Ease.OutCubic);
+    }
+
+    public void ResetText()
+    {
+        Text.SetText(_defaultText);
+    }
+
+    private string _defaultText;
+
+    private void Start()
+    {
+        _defaultText = Text.text;
+    }
+
+    public void CorruptText()
+    {
+        var textString = Text.text;
+        var newText = "";
+        foreach (var word in textString.Split(' '))
+        {
+            if (Random.value < TextCorruptionValue)
+            {
+                var newWord = (new string(' ', word.Length + 1));
+                newText += newWord;
+            }
+            else
+            {
+                var text = word;
+                if (Random.value > TextCorruptionValue * 2.3f)
+                {
+                    var rand =  new string(text.ToCharArray().OrderBy(x=>Guid.NewGuid()).ToArray());
+                    newText += rand + " ";
+                }
+                else
+                {
+                    newText += word + " ";
+                }
+
+            }
+        }
+
+        Text.SetText(newText);
     }
 }
