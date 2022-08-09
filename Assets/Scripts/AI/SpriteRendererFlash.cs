b8cf795d50c7c3ad0e396ba48b907c3222f65453
relisqu <kopkopkop47@gmail.com>
2022-08-10 01:53:06 +0300

    added base of flash shader

---
+++ Assets/Scripts/AI/SpriteRendererFlash.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace AI
+{
+    public class SpriteRendererFlash : MonoBehaviour
+    {
+        [SerializeField] private Color FlashColor;
+        [SerializeField] private float FlashDuration;
+        [SerializeField] private SpriteRenderer SpriteRenderer;
+
+        private void OnEnable()
+        {
+            SpriteRenderer.material.color = _defaultColor;
+        }
+
+        public void Flash()
+        {
+            StopAllCoroutines();
+            StartCoroutine(GenerateFlash());
+        }
+
+        IEnumerator GenerateFlash()
+        {
+            SpriteRenderer.material.SetColor("FlashColor",FlashColor); 
+            SpriteRenderer.material.SetInt("IsEnabled", 1);
+            yield return new WaitForSeconds(FlashDuration);
+            SpriteRenderer.material.SetInt("IsEnabled", 0);
+        }
+
+        private void Start()
+        {
+            _defaultColor = SpriteRenderer.material.color;
+        }
+
+        private Color _defaultColor;
+    }
+}
