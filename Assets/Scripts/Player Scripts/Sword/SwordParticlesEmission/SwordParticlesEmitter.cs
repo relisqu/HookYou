24dd883a91b882aeed137850e233371668693ee3
relisqu <kopkopkop47@gmail.com>
2022-10-10 23:47:18 +0300

    movd particles to the bottom layer, reduced its amount

---
+++ Assets/Scripts/Player Scripts/Sword/SwordParticlesEmission/SwordParticlesEmitter.cs
@@ -0,0 +1,23 @@
+using System;
+using Assets.Scripts;
+using UnityEngine;
+
+namespace Player_Scripts.Sword.SwordParticlesEmission
+{
+    public class SwordParticlesEmitter : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem.MinMaxGradient ParticlesColorScheme;
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out SwordAttack sword))
+            {
+                if (!sword.IsAttacking) return;
+                ParticleSystem.MainModule settings = sword.EmitParticles.main;
+                ParticlesColorScheme.mode = ParticleSystemGradientMode.RandomColor;
+                settings.startColor = ParticlesColorScheme;
+                sword.EmitParticles.Emit(6);
+            }
+        }
+    }
+}
