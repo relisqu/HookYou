761aa264c66ecbc63ab0db44dc29994dd9ec36a7
relisqu <kopkopkop47@gmail.com>
2022-10-29 02:50:45 +0300

    finally all fixes for today

--- Assets/Scripts/Player Scripts/Sword/SwordParticlesEmission/SwordParticlesEmitter.cs
+++ Assets/Scripts/Player Scripts/Sword/SwordParticlesEmission/SwordParticlesEmitter.cs
@@ -7,16 +7,17 @@ namespace Player_Scripts.Sword.SwordParticlesEmission
     public class SwordParticlesEmitter : MonoBehaviour
     {
         [SerializeField] private ParticleSystem.MinMaxGradient ParticlesColorScheme;
+        [SerializeField] private float ParticlesMultiplier=1;
 
         private void OnTriggerStay2D(Collider2D other)
         {
             if (other.TryGetComponent(out SwordAttack sword))
             {
                 if (!sword.IsAttacking) return;
-                ParticleSystem.MainModule settings = sword.EmitParticles.main;
+                var settings = sword.EmitParticles.main;
                 ParticlesColorScheme.mode = ParticleSystemGradientMode.RandomColor;
                 settings.startColor = ParticlesColorScheme;
-                sword.EmitParticles.Emit(10);
+                sword.EmitParticles.Emit((int) (10*ParticlesMultiplier));
             }
         }
     }
