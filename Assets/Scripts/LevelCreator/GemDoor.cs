7928aac8a258215ce07047f2e217f4ef1dedbf3f
relisqu <kopkopkop47@gmail.com>
2022-11-23 17:33:49 +0300

    added gemdoor system

--- Assets/Scripts/LevelCreator/GemDoor.cs
+++ Assets/Scripts/LevelCreator/GemDoor.cs
@@ -2,30 +2,36 @@ using System;
 using Assets.Scripts.LevelCreator;
 using Player_Scripts;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace LevelCreator
 {
     public class GemDoor : MonoBehaviour
     {
-        [SerializeField]private Door Door;
-        [SerializeField]private Level Level;
-        [SerializeField]private int GemCount;
+        [SerializeField] private Door Door;
+        [SerializeField] private Level Level;
+        [FormerlySerializedAs("GemCount")] [SerializeField] private int RequiredGemCount;
 
         private void OnEnable()
         {
             Door.ManuallyClose();
         }
 
-        private void OnTriggerEnter2D(Collider2D other)
+
+        public void TryOpenGemDoor()
         {
-            if(!other.TryGetComponent(out Player _)) return;
-            if (GemCount <= PlayerStats.Instance.GetGemsCount())
+            if (RequiredGemCount <= PlayerStats.Instance.GetGemsCount())
             {
                 if (Level.IsCompleted && !Door.IsCurrentlyOpened)
                 {
                     Door.ManuallyOpen();
                 }
             }
+        }
+
+        public int GetRequiredGemCount()
+        {
+            return RequiredGemCount;
         }
     }
 }
--- Assets/Scripts/Player Scripts/PropsCollector.cs
+++ Assets/Scripts/Player Scripts/PropsCollector.cs
@@ -11,28 +11,30 @@ namespace Player_Scripts
 {
     public class PropsCollector : MonoBehaviour
     {
-       
-        [SerializeField]private GameObject GemObject;
-        [SerializeField]private Vector3 Offset;
-        [SerializeField]private Ease ease;
-        [Min(0.1f)][SerializeField]private float CrystalSpeed;
-        [SerializeField]private float CrystalDistance;
-        [SerializeField]private UpdateGemText UpdateGemText;
+        [SerializeField] private GameObject GemObject;
+        [SerializeField] private Vector3 Offset;
+        [SerializeField] private Ease ease;
+        [Min(0.1f)] [SerializeField] private float CrystalSpeed;
+        [SerializeField] private float CrystalDistance;
+        [SerializeField] private UpdateGemText UpdateGemText;
+        [SerializeField] public static Action OnGemCollect;
 
         public void CollectGem()
         {
-            var gem=Instantiate(GemObject,transform.position+Offset,transform.rotation, transform.root);
-            gem.transform.DOLocalMoveY(CrystalDistance, 1 / CrystalSpeed).SetEase(ease).OnComplete(()=>CrystalDisappear(gem));
-            
+            var gem = Instantiate(GemObject, transform.position + Offset, transform.rotation, transform.root);
+            gem.transform.DOLocalMoveY(CrystalDistance, 1 / CrystalSpeed).SetEase(ease)
+                .OnComplete(() => CrystalDisappear(gem));
+
             PlayerStats.Instance.AddGemsCount(1);
             UpdateGemText.UpdateText();
+            OnGemCollect?.Invoke();
         }
 
         private void CrystalDisappear(GameObject gem)
         {
-            DOTween.To(()=> gem.GetComponentInChildren<Light2D>().intensity, x=> gem.GetComponentInChildren<Light2D>().intensity = x, 0, 0.05f);
+            DOTween.To(() => gem.GetComponentInChildren<Light2D>().intensity,
+                x => gem.GetComponentInChildren<Light2D>().intensity = x, 0, 0.05f);
             gem.GetComponentInChildren<SpriteRenderer>().DOColor(Color.clear, 0.1f).OnComplete(() => Destroy(gem));
         }
-
     }
 }
