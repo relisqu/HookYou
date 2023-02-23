5ad4c16dd193cd518cab30dd49529a73e5547cbc
Kopeikina Anna <kopkopkop47@gmail.com>
2023-02-23 12:09:46 +0300

    btoke camera

--- Assets/Scripts/Player Scripts/RotateAppearance.cs
+++ Assets/Scripts/Player Scripts/RotateAppearance.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class RotateAppearance : MonoBehaviour
 {
-    private Camera camera;
+    [FormerlySerializedAs("camera")] [SerializeField] private Camera Camera;
 
     private void Start()
     {
-        camera = Camera.main;
     }
 
     private void Update()
     {
         //if (!Input.GetMouseButtonDown(0)) return;
-        var lookRotation = (camera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        var lookRotation = (Camera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
         transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, -Vector2.SignedAngle(lookRotation, Vector3.right)));
+    }
+
+    private void OnDrawGizmos()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z=Camera.nearClipPlane;
+        Gizmos.DrawCube(Camera.ScreenToWorldPoint(mousePos), Vector3.one);
     }
 }
